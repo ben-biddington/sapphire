2306c2644556977f8beccbf91de870fec623cd26
Ben Biddington <ben.biddington@gmail.com>
2020-12-11 20:58:32 +1300

    Adjust type hints

--- internal/google-calendar.ts
+++ internal/google-calendar.ts
@@ -4,16 +4,16 @@ export async function summary(auth: any, opts: any = {}) {
     const events = await listEvents(auth, opts);
 
     // [i] https://developers.google.com/calendar/v3/reference/events
-    return events.map(event:any => ({
+    return events.map((event:any) => ({
         sumary: event.summary,
         creator: event.creator.email,
         organizer: event.organizer.email,
         colorId: event.colorId,
-    }))
+    }));
 }
 
 // https://developers.google.com/calendar/quickstart/nodejs
-export function listEvents(auth: any, opts: any = {}) {
+export function listEvents(auth: any, opts: any = {}) : Promise<Array<any>> {
     const { id, limit = 10 } = opts;
     const calendar = google.calendar({version: 'v3', auth});
 
